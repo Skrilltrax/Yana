74bbf069baf31187adeffe5e1d04fdcfde3bdab8
Aditya Wasan <adityawasan55@gmail.com>
2019-08-17 14:26:47 +0530

    Build system update
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,18 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0-alpha05")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
-        classpath("io.realm:realm-gradle-plugin:5.14.0")
+        classpath(Libs.com_android_tools_build_gradle)
+        classpath(Libs.kotlin_gradle_plugin)
+        classpath(Libs.realm_gradle_plugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+
+plugins {
+    id("de.fayard.buildSrcVersions") version "0.4.1"
+    id("se.patrikerdes.use-latest-versions") version "0.2.12"
 }
 
 allprojects {
@@ -26,5 +31,9 @@ allprojects {
 tasks {
     val clean by registering(Delete::class) {
         delete(buildDir)
+    }
+    val updateGradle by registering(Wrapper::class) {
+        gradleVersion = Versions.gradleLatestVersion
+        distributionType = Wrapper.DistributionType.ALL
     }
 }
