8c4c4411eafac26cd01271a02c907f2ec5f6a0d9
Aditya Wasan <adityawasan55@gmail.com>
2019-09-14 19:16:41 +0530

    Revert "Add menu for bottom appbar"
    This reverts commit f51b0db50c3a1a2217c1d641cb9f55cce5948af7.

--- app/src/main/java/me/skrilltrax/notes/ui/activities/MainActivity.kt
+++ app/src/main/java/me/skrilltrax/notes/ui/activities/MainActivity.kt
@@ -3,15 +3,10 @@ package me.skrilltrax.notes.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
-import android.view.animation.LinearInterpolator
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,9 +17,8 @@ import me.skrilltrax.notes.*
 import me.skrilltrax.notes.databinding.ActivityMainBinding
 import me.skrilltrax.notes.ui.fragments.NotesListFragment
 import me.skrilltrax.notes.ui.viewmodel.MainActivityViewModel
-import org.jetbrains.anko.imageResource
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val viewModel: MainActivityViewModel by lazy { ViewModelProvider(this).get(MainActivityViewModel::class.java) }
     private lateinit var binding: ActivityMainBinding
@@ -38,66 +32,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, NavController.On
         host = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
         navController = host.navController
-        navController.addOnDestinationChangedListener(this)
-        setSupportActionBar(binding.bottomAppbar)
-//        setObservers()
+        setObservers()
         setListeners()
     }
 
-    fun setListeners() {
-        binding.fab.setOnClickListener(this)
-    }
+    private fun setListeners() {
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.noteFragment)
+        }
 
-    fun changeToNoteFragment() {
-        binding.fab.animate().apply {
-            duration = 2000
-            alpha(0f)
-            interpolator = LinearInterpolator()
-        }.start()
-        binding.fab.setImageResource(R.drawable.ic_check_white_24dp)
-        binding.fab.animate().apply {
-            duration = 2000
-            alpha(1.0f)
-            interpolator = LinearInterpolator()
-        }.start()
-//        binding.bottomAppbar.replaceMenu()
-    }
-
-    override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
-        when (destination.id) {
-            R.id.noteFragment -> {
-                changeToNoteFragment()
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.noteFragment -> {
+                    binding.fab.setImageResource(R.drawable.ic_check_white_24dp)
+                }
             }
         }
+    }
+
+    private fun setObservers() {
+
     }
 
     override fun onClick(view: View?) {
         val currentDestination = navController.currentDestination
         if (currentDestination != null) {
             when (currentDestination.id) {
-                R.id.notesListFragment -> {
-                    navController.navigate(R.id.noteFragment)
-                    changeToNoteFragment()
+                R.id.nav_host_fragment -> {
+
                 }
                 R.id.noteFragment -> {
+
                 }
             }
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        when (host.navController.currentDestination?.id) {
-            R.id.noteFragment -> {
-//                binding.bottomAppbar.replaceMenu()
-            }
-            R.id.notesListFragment -> {
-                menuInflater.inflate(R.menu.menu_note, menu)
-            }
-        }
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-    }
 }
+
