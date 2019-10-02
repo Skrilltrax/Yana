43985c6246ba4c4abff06f1c1d96bef5a1f3b7bf
Aditya Wasan <adityawasan55@gmail.com>
2019-10-02 19:32:42 +0530

    use theme color in android:fillColor
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/src/main/java/me/skrilltrax/notes/MainActivityRouter.kt
+++ app/src/main/java/me/skrilltrax/notes/MainActivityRouter.kt
@@ -5,6 +5,15 @@ import me.skrilltrax.notes.databinding.ActivityMainBinding
 
 class MainActivityRouter(private val binding: ActivityMainBinding) {
 
+    private fun routeToNoteFragment() {
+        AnimUtils.animateFAB(binding.fab, R.drawable.ic_check_white_24dp)
+        binding.bottomAppbar.replaceMenu(R.menu.menu_note)
+    }
+    private fun routeToNoteListFragment() {
+        AnimUtils.animateFAB(binding.fab, R.drawable.ic_add_white_24dp)
+        binding.bottomAppbar.replaceMenu(R.menu.menu_note)
+    }
+
     fun handleNavigation(navController: NavController) {
         val currentDestination = navController.currentDestination
         if (currentDestination != null) {
@@ -20,15 +29,6 @@ class MainActivityRouter(private val binding: ActivityMainBinding) {
                 }
             }
         }
-    }
-
-    private fun routeToNoteFragment() {
-        AnimUtils.animateFAB(binding.fab, R.drawable.ic_check_white_24dp)
-        binding.bottomAppbar.replaceMenu(R.menu.menu_note)
-    }
-    private fun routeToNoteListFragment() {
-        AnimUtils.animateFAB(binding.fab, R.drawable.ic_add_white_24dp)
-        binding.bottomAppbar.replaceMenu(R.menu.menu_note)
     }
 
     interface FabClickListener {
